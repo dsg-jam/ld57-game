6eaf2260c754930646e929c877aaf42add1520c0
mineiwik <29404520+mineiwik@users.noreply.github.com>
2025-04-06 20:58:42 +0200

    add count badge to items

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -5,6 +5,7 @@ extends Node
 @export var _tile_manager: M_TileManager
 
 var _item_button_prefab = preload("res://prefabs/hud/item_button.tscn")
+var _item_buttons: Dictionary[Global.ItemType, ItemButton]
 var _available_items: Dictionary[Global.ItemType, int] = {
 	Global.ItemType.MIRROR: 2
 }
@@ -23,6 +24,7 @@ func _input(event):
 	elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
 		# Delete item if applicable
 		self._remove_item()
+	self._update_counters()
 
 func _setup_ui():
 	for item_type in Global.ALL_ITEM_TYPES:
@@ -30,6 +32,11 @@ func _setup_ui():
 		_item_container.add_child(item_button)
 		item_button.setup(item_type)
 		item_button.button_pressed.connect(_on_item_button_pressed)
+		self._item_buttons.set(item_type, item_button)
+
+func _update_counters():
+	for item_type in self._item_buttons.keys():
+		self._item_buttons[item_type].update_counter(self._available_items[item_type])
 
 func _consume_item(item_type: Global.ItemType) -> bool:
 	if not (item_type in self._available_items):
--- scripts/ui/item_button.gd
+++ scripts/ui/item_button.gd
@@ -1,14 +1,19 @@
-class_name ItemButton extends Panel
+class_name ItemButton extends Control
 
-@export var button: Button
+@export var _button: Button
+@export var _counter_label: Label
 
 signal button_pressed
+signal counter_updated
 
 var _type: Global.ItemType
 
 func setup(type: Global.ItemType) -> void:
 	self._type = type
-	button.text = Global.ItemTypeToName[self._type]
+	self._button.text = Global.ItemTypeToName[self._type]
 
 func _on_button_pressed() -> void:
 	self.button_pressed.emit(self._type)
+
+func update_counter(counter: int) -> void:
+	self._counter_label.text = str(counter)
